d9aaee983518ded08d46445f97ff88298878e5fc
Perseu Alves <perseu.pereira@fatec.sp.gov.br>
2022-04-17 09:55:56 -0300

    Projeto inicializado

---
+++ src/main/java/br/com/pereira/LojaDeDoces/LojaDeDocesApplication.java
@@ -0,0 +1,13 @@
+package br.com.pereira.LojaDeDoces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeDocesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeDocesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pereira/LojaDeDoces/LojaDeDocesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pereira.LojaDeDoces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeDocesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
