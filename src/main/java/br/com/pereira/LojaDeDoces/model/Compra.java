87cd22dddf358f1b14fca0d30873db3079920965
Perseu Alves <perseu.pereira@fatec.sp.gov.br>
2022-05-01 00:29:02 -0300

    Hotfix na classe de Compra

--- src/main/java/br/com/pereira/LojaDeDoces/model/Compra.java
+++ src/main/java/br/com/pereira/LojaDeDoces/model/Compra.java
@@ -8,6 +8,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Range;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -42,5 +44,9 @@ public class Compra {
 	@JoinColumn(name = "produto_id")
 	@NotNull
 	private Produto produto;
+	@Column
+	@NotNull
+	@Range(min=0, max=1)
+	private int compra_finalizada;
 	
 }
