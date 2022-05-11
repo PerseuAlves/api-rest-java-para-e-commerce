23e4b775072c4a9815b10bfba78c01c02189e25f
Perseu Alves <perseu.pereira@fatec.sp.gov.br>
2022-05-10 23:37:26 -0300

    Implementacao da logica de recebimento e delecao de arquivos txt no projeto, alem de alguns hotfixes

--- src/main/java/br/com/pereira/LojaDeDoces/controller/ImagemController.java
+++ src/main/java/br/com/pereira/LojaDeDoces/controller/ImagemController.java
@@ -1,8 +1,10 @@
 package br.com.pereira.LojaDeDoces.controller;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
@@ -33,14 +35,23 @@ public class ImagemController {
 	private ImagemService imagemService;
 	
 	@GetMapping("/imagem/{ImagemId}")
-	public ResponseEntity<Imagem> getImagem(
+	public ResponseEntity<String> getImagem(
 			@PathVariable(value = "ImagemId") String imagemId) {
 		
 		Imagem imagem = imagemService.findImagem(imagemId);
+		Path diretorioAtual = Paths.get("").toAbsolutePath();
 		
-		// TODO
-		
-		return ResponseEntity.ok().body(imagem);
+		try {
+			String arquivoPath = diretorioAtual.normalize().toString() + File.separator + "Documents" + File.separator + "jee-2021-06" + File.separator + "Projetos" + File.separator + "LojaDeDoces" + File.separator + "arquivos" + File.separator + imagem.getId();
+			Path path = Paths.get(arquivoPath);
+			
+		    BufferedReader reader = Files.newBufferedReader(path);
+		    String line = reader.readLine();
+    	    
+    	    return ResponseEntity.status(HttpStatus.OK).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"" + line + "\"}");
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao ler arquivo da imagem no servidor\"}");
+		}
 	}
 	
 	@RequestMapping(value = "/imagem/{ImagemId}", method = RequestMethod.POST, consumes = MediaType.TEXT_PLAIN_VALUE)
@@ -50,6 +61,7 @@ public class ImagemController {
 		try {
 			@SuppressWarnings("unused")
 			Imagem imagem = imagemService.findImagem(new Imagem(nomeDoArquivo).getId());
+			
 			return ResponseEntity.status(HttpStatus.CONFLICT).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Imagem já cadastrada\"}");
 		} catch (ResourceNotFoundException ex) {
 			Path diretorioAtual = Paths.get("").toAbsolutePath();
@@ -65,7 +77,7 @@ public class ImagemController {
 					writer.close();
 				}
 			} catch (IOException e) {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao criar arquivo da imagem no server\"}");
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao criar arquivo da imagem no servidor\"}");
 			}
 			
 			imagemService.save(new Imagem(nomeDoArquivo));
@@ -76,34 +88,41 @@ public class ImagemController {
 	@RequestMapping(value = "/imagem/{ImagemId}", method = RequestMethod.PUT, consumes = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> putImagem(@PathVariable(value = "ImagemId") String nomeDoArquivo, @RequestBody String arquivoEmString) {
 		
+		Path diretorioAtual = Paths.get("").toAbsolutePath();
+		
 		try {
-			Path diretorioAtual = Paths.get("").toAbsolutePath();
-			
-			try {
-				String path = diretorioAtual.normalize().toString() + File.separator + "Documents" + File.separator + "jee-2021-06" + File.separator + "Projetos" + File.separator + "LojaDeDoces" + File.separator + "arquivos";
-				File novoArquivo = new File(path + File.separator + nomeDoArquivo);
-			
-				if(novoArquivo.exists()) {
-					FileWriter writer = new FileWriter(path + File.separator + nomeDoArquivo, false);
-					
-					writer.write(arquivoEmString);
-					writer.close();
-				}
-			} catch (IOException e) {
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao criar arquivo da imagem no server\"}");
+			String path = diretorioAtual.normalize().toString() + File.separator + "Documents" + File.separator + "jee-2021-06" + File.separator + "Projetos" + File.separator + "LojaDeDoces" + File.separator + "arquivos";
+			File novoArquivo = new File(path + File.separator + nomeDoArquivo);
+		
+			if(novoArquivo.exists()) {
+				FileWriter writer = new FileWriter(path + File.separator + nomeDoArquivo, false);
+				
+				writer.write(arquivoEmString);
+				writer.close();
 			}
-			
-			imagemService.save(new Imagem(nomeDoArquivo));
-			return ResponseEntity.status(HttpStatus.OK).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Imagem atualizada com sucesso\"}");
-		} catch (Exception ex) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao criar arquivo da imagem no server\"}");
+		} catch (IOException e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao criar arquivo da imagem no servidor\"}");
 		}
+		
+		imagemService.save(new Imagem(nomeDoArquivo));
+		return ResponseEntity.status(HttpStatus.OK).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Imagem atualizada com sucesso\"}");
     }
 	
 	@DeleteMapping("/imagem")
 	public ResponseEntity<String> deleteImagem(@Valid @RequestBody Imagem i) {
 		
-		imagemService.delete(i);
-		return ResponseEntity.status(HttpStatus.OK).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Imagem deletada com sucesso\"}");
+		Path diretorioAtual = Paths.get("").toAbsolutePath();
+		
+		String path = diretorioAtual.normalize().toString() + File.separator + "Documents" + File.separator + "jee-2021-06" + File.separator + "Projetos" + File.separator + "LojaDeDoces" + File.separator + "arquivos";
+		File novoArquivo = new File(path + File.separator + i.getId());
+		
+		if(novoArquivo.exists()) {
+			novoArquivo.delete();
+			imagemService.delete(i);
+			
+			return ResponseEntity.status(HttpStatus.OK).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Imagem deletada com sucesso\"}");
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"timestamp\":\"" + Instant.now() + "\",\"message\":\"Erro ao deletar arquivo da imagem no servidor\"}");
+		}
     }
 }
